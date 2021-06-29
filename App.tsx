9d63cb567148f65d54b459ba9b6b2042bbe9c574
Mateus Satoh <mateussatoh@gmail.com>
2021-06-28 21:03:08 -0300

    add: background gradient / fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
 
 import { Signin } from "./src/screens/Signin";
+import { Background } from "./src/components/Background";
 
 const App = () => {
-  return <Signin />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Signin />
+    </Background>
+  );
 };
 
 export default App;
--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image } from "react-native";
 import { styles } from "./styles";
 
 import { ButtonIcon } from "../../components/ButtonIcon";
@@ -9,17 +9,11 @@ import Img from "../../assets/illustration.png";
 export const Signin = () => {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
       <Image source={Img} style={styles.image} resizeMode="stretch" />
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {"\n"}
-          suas jogatinas {"\n"}
-          facilmente
+          Conecte-se {"\n"}e organize suas {"\n"}
+          jogatinas
         </Text>
         <Text style={styles.subtitle}>
           Crie grupos para jogar seus games {"\n"}
