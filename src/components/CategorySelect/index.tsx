61be6c285f7df0383066c862d1745fd10fd7991e
Mateus Satoh <mateussatoh@gmail.com>
2021-06-29 17:57:20 -0300

    add: category select

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+import { Category } from "../Category";
+
+import { styles } from "./styles";
+import { categories } from "../../utils/categories";
+
+type Props = {
+  categorySelected: string;
+  setCategory: (category: string) => void;
+};
+
+export const CategorySelect = ({ categorySelected, setCategory }: Props) => {
+  return (
+    <ScrollView
+      horizontal
+      style={styles.container}
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingRight: 40 }}
+    >
+      {categories.map((category) => (
+        <Category
+          key={category.id}
+          title={category.title}
+          icon={category.icon}
+          checked={category.id === categorySelected}
+          onPress={() => setCategory(category.id)}
+        />
+      ))}
+    </ScrollView>
+  );
+};
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 
 import { styles } from "./styles";
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
 
 export const Home = () => {
+  const [category, setCategory] = useState("");
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory("") : setCategory(categoryId);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
+      </View>
+      <View>
+        <CategorySelect
+          setCategory={handleCategorySelect}
+          categorySelected={category}
+        />
       </View>
     </View>
   );
